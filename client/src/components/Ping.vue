8bd64971259f6e72e29a48592f5466210100ba6c
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-07-21 13:26:17 +0900

    FEATURE - /ping page

---
+++ client/src/components/Ping.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/ping',
+      name: 'Ping',
+      component: Ping,
+    },
+  ],
+});
+</script>
