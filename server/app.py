a5f681a9fb0885a248a127a3c44672c247f90192
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-07-21 11:34:41 +0900

    FEATURE - comments

--- server/app.py
+++ server/app.py
@@ -2,8 +2,10 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 DEBUG = True
+# app 인스턴스화
 app = Flask(__name__)
 app.config.from_object(__name__)
+# Cross Origin 설정
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
