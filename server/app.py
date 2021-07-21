f9cbd70c9c5a5b822f5695137ae7c7f1d090407b
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-07-21 16:12:03 +0900

    FEATURE - PUT Route

--- server/app.py
+++ server/app.py
@@ -1,5 +1,8 @@
+import uuid
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+
 
 DEBUG = True
 # app 인스턴스화
@@ -8,25 +11,35 @@ app.config.from_object(__name__)
 # Cross Origin 설정
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-
 # Books list 추가
 BOOKS = [
     {
+        'id': uuid.uuid4().hex,
         'title': 'On the Road',
         'author': 'Jack Kerouac',
         'read': True
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'Harry Potter and the Philosopher\'s Stone',
         'author': 'J. K. Rowling',
         'read': False
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'Green Eggs and Ham',
         'author': 'Dr. Seuss',
         'read': True
     }
 ]
+
+
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+    return False
 
 
 @app.route('/ping', methods=['GET'])
@@ -40,6 +53,7 @@ def all_books():
     if request.method == 'POST':
         post_data = request.get_json()
         BOOKS.append({
+            'id': uuid.uuid4().hex,
             'title': post_data.get('title'),
             'author': post_data.get('author'),
             'read': post_data.get('read')
@@ -47,6 +61,22 @@ def all_books():
         response_object['message'] = 'Book added!'
     else:
         response_object['books'] = BOOKS
+    return jsonify(response_object)
+
+
+@app.route('/books/<book_id>', methods=['PUT'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(book_id)
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book updated!'
     return jsonify(response_object)
 
 
