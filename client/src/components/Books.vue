c61a418c6033290efedb4e7620d2573640f3a4f8
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-07-21 17:02:39 +0900

    FEATURE - DELETE Route

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -33,7 +33,12 @@
                           @click="editBook(book)">
                       Update
                   </button>
-                  <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                  <button
+                          type="button"
+                          class="btn btn-danger btn-sm"
+                          @click="onDeleteBook(book)">
+                      Delete
+                  </button>
                 </div>
               </td>
             </tr>
@@ -232,6 +237,23 @@ export default {
       this.$refs.editBookModal.hide();
       this.initForm();
       this.getBooks(); // why?
+    },
+    removeBook(bookID) {
+      const path = `http://localhost:5000/books/${bookID}`;
+      axios.delete(path)
+        .then(() => {
+          this.getBooks();
+          this.message = 'Book removed!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+          this.getBooks();
+        });
+    },
+    onDeleteBook(book) {
+      this.removeBook(book.id);
     },
   },
   created() {
